351d3a562a8fce7149081d1ca0fa15a112323e16
atul-bhatt <atulbhatt61@gmail.com>
2019-01-18 13:53:36 +0530

    swap file created

---
+++ swap.py
@@ -0,0 +1,11 @@
+# python program to swap values of two integers
+
+if __name__ == __main__:
+	x = 14
+	y = 16
+
+	print("values before swapping : x = " + str(x) + "   y = " + str(y))
+
+	# swapping...
+
+	print("values after swapping : x = " + str(x) + "   y = " + str(y))	
